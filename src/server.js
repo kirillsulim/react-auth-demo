8df912173bb50fb1b9769e38be05e8eb9397067b
Kirill Sulim <kirillsulim@gmail.com>
2023-06-15 19:00:52 +0500

    Add token check

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const { SignJWT } = require('jose');
+const { SignJWT, jwtVerify } = require('jose');
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -24,8 +24,19 @@ async function issueToken(user) {
     })
         .setProtectedHeader({alg})
         .setIssuedAt()
-        .setExpirationTime('2h')
+        .setExpirationTime('1h')
         .sign(secret);
+}
+
+async function checkToken(token) {
+    try {
+        const {payload, protectedHeader} = await jwtVerify(token, secret);
+        return true;
+    } 
+    catch (error) {
+        console.log(error);
+        return false;
+    }
 }
 
 server.post("/registration", async (req, res) => {
@@ -89,6 +100,38 @@ const disallowProtectedResources = (req, res, next) => {
 }
 server.use(disallowProtectedResources);
 
+const anonResources = [
+    "/registration",
+    "/login"
+]
+const checkAuth = async (req, res, next) => {
+    if (anonResources.includes(req.path.toLowerCase())) {
+        next();
+    } else {
+        const authHeader = req.get("Authorization");
+        if (!authHeader) {
+            res.status(401);
+            res.send("Missing authorization header");
+            return;
+        }
+
+        const [authScheme, token] = authHeader.split(" ", 2);
+        if (authScheme !== "Bearer" || !token) {
+            res.status(401);
+            res.send("Incorrect authorization info. Expected bearer token.");
+            return;
+        }
+
+        if (!await checkToken(token)) {
+            res.status(401);
+            res.send("Invalid token");
+            return;
+        }
+        
+        next();
+    }
+}
+server.use(checkAuth);
 
 server.use(router)
 
